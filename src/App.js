f80d06733bb9923e41241521ff5c03aa3bf2670f
ielberPellegrini <ielberx10@gmail.com>
2025-02-12 15:00:03 -0300

    Loja-de-Livros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './componentes/Header';
 import styled from 'styled-components';
+import Pesquisa  from './componentes/Pesquisa';
 
 const AppContainer = styled.div`
     width: 100vw;
@@ -11,7 +12,8 @@ const AppContainer = styled.div`
 function App() {
   return (
     <AppContainer>
-      <Header/>
+      <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
