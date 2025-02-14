ca3f497fe613a37393cc40cc3fc95beb63e67c32
ielberPellegrini <ielberx10@gmail.com>
2025-02-14 13:16:31 -0300

    Loja-de-Livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+
+export const livros = [
+    {nome: 'Quem lidera, nem sempre é lider', id: 1, src:livro },
+    {nome: 'Um dia é sempre igual', id: 2, src:livro2 },
+    {nome: 'Quem faz, faz 2x', id: 3, src:livro },
+    {nome: 'Um Usuário de JS', id: 4, src:livro2 },
+]
+
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,38 +1,70 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
-        background-image: linear-gradient(90deg, #ff0000 35%, #2d302d);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    background-image: linear-gradient(90deg, #ff0000 35%, #2d302d);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
 `
 
-function Pesquisa () {
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+function Pesquisa() {
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+
     return (
         <PesquisaContainer>
-            <Titulo>Qual o Livro você quer ler?</Titulo>
-            <Subtitulo>Encontre seu Livro em nosso site</Subtitulo>
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
-                placeholder="Digite o nome do Livro"
+                placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
 
-export default Pesquisa;
+export default Pesquisa
